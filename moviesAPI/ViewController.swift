e025aaef35c3f9c1371decb4fa1d3b00f4986618
allwsaa <bekbosynova.alima@inbox.ru>
2024-11-14 21:30:08 +0500

    Initial Commit

---
+++ moviesAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moviesAPI
+//
+//  Created by ntvlbl on 14.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
